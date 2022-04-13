e4079b89abd96635b8a7d67ae515b3ad4cd86b97
Kyle Bastien <mcnuttkyle93@gmail.com>
2022-04-12 21:07:13 -0700

    Doing week 2 assignment

--- app/src/androidTest/java/com/example/ad340kylebastienweeklyassignments/MainActivityTest.java
+++ app/src/androidTest/java/com/example/ad340kylebastienweeklyassignments/MainActivityTest.java
@@ -1,13 +1,20 @@
 package com.example.ad340kylebastienweeklyassignments;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import android.widget.DatePicker;
+
+import androidx.test.espresso.contrib.PickerActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,11 +23,24 @@ import org.junit.runner.RunWith;
 public class MainActivityTest {
 
     @Rule
-    public ActivityScenarioRule<MainActivity> mainActivityRule = new ActivityScenarioRule<>(MainActivity.class);
+    public ActivityScenarioRule<MainActivity> mainActivityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
-    public void displaysHelloWorld() {
-        onView(withId(R.id.hello_world)).check(matches(withText("Hello World!")));
+    public void canGoThroughForm() {
+        onView(withId(R.id.nameField)).perform(typeText("Kyle Bastien"));
+        onView(withId(R.id.username)).perform(typeText("kylebastien"));
+        onView(withId(R.id.emailAddress)).perform(typeText("foo@bar.com"));
+
+        onView(withId(R.id.dobButton)).perform(click());
+
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName())))
+                .perform(PickerActions.setDate(2000, 2 + 1, 5));
+
+        onView(withId(R.id.submitButton)).perform(click());
+
+        onView(withId(R.id.welcomeText))
+                .check(matches(withText("Thank you for signing up kylebastien!")));
     }
 
 }
---
+++ app/src/main/java/com/example/ad340kylebastienweeklyassignments/Constants.java
@@ -0,0 +1,6 @@
+package com.example.ad340kylebastienweeklyassignments;
+
+public class Constants {
+    public static final String USERNAME_KEY = "username";
+    public static final String SELECTED_DOB = "selectedDob";
+}
