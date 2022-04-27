3e5ca8b1a0bfd02875e363e4a403568b006cb960
Kyle Bastien <mcnuttkyle93@gmail.com>
2022-04-26 20:15:11 -0700

    Doing the Week 4 assignment

---
+++ app/src/main/java/com/example/ad340kylebastienweeklyassignments/ProfileData.java
@@ -0,0 +1,31 @@
+package com.example.ad340kylebastienweeklyassignments;
+
+public class ProfileData {
+    String name;
+    int age;
+    String occupation;
+    String description;
+
+    public ProfileData(String name, int age, String occupation, String description) {
+        this.name = name;
+        this.age = age;
+        this.occupation = occupation;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
